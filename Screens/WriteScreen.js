b5632cd585472de5753053cf9676264c2b6b6280
Khushi <60261630+Khushi241@users.noreply.github.com>
2020-07-17 13:45:45 +0530

    Add files via upload

---
+++ Screens/WriteScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default class WriteScreen extends React.Component{
+    render(){
+        return(
+            <View>
+               <Text> Write Screen </Text>
+            </View>
+        )
+    }
+}
+
